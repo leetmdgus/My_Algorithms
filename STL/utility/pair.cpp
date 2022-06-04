3528cf4474f9cb528337a0f788b70379008d1a1d
leetmdgus <lee_tmdgus@naver.com>
2022-06-04 19:28:33 +0900

    add comment

--- STL/utility/pair.cpp
+++ STL/utility/pair.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 
 int main() {
+
+  // make_pair을 이용하여 타입을 지정하지 않아도 자동으로 추론할 수 있다. 
   std::pair<const char*, double> charDoub("string", 3.14);
   std::pair<const char*, double> charDoub2 = std::make_pair("string", 3.14);
   auto charDoub3 = std::make_pair("string", 3.14);
