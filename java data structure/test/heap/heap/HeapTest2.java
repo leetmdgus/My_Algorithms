81f495733dd62ff94454cad1cc87fe730c1d1877
leetmdgus <lee_tmdgus@naver.com>
2022-08-10 16:51:08 +0900

    add TestCode

--- java data structure/test/heap/heap/HeapTest2.java
+++ java data structure/test/heap/heap/HeapTest2.java
@@ -9,15 +9,20 @@ class HeapTest2 {
 
     @Test
     void add() {
-        for(int i = 1; i<=10; i++) {
+        for(int i = 9; i>2; i--) {
             heap.add(i);
         }
-        assertEquals(heap.toString(), "[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]");
+        assertEquals(heap.toString(), "[0, 3, 6, 4, 9, 7, 8, 5]");
     }
 
     @Test
     void remove() {
-
+        for(int i = 9; i>2; i--) {
+            heap.add(i);
+        }
+        heap.remove();
+        heap.remove();
+        assertEquals(heap.toString(), "[0, 5, 6, 8, 9, 7]");
 
     }
 }
